d4c12e1c55b293441cfb528c6920f25de9fdbc2c
WN <35946230+iceshellwang@users.noreply.github.com>
2024-06-03 01:57:06 +1000

    convert speech to text

--- src/components/AudioRecorder.jsx
+++ src/components/AudioRecorder.jsx
@@ -1,9 +1,15 @@
 import { useState, useRef, useEffect } from "react";
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 import './Recorder.css';
 
 const mimeType = "audio/webm";
 
 const AudioRecorder = () => {
+    const {
+        transcript,
+        resetTranscript,
+      } = useSpeechRecognition();
+    
 	const [permission, setPermission] = useState(false);
 
 	const mediaRecorder = useRef(null);
@@ -26,6 +32,9 @@ const AudioRecorder = () => {
 				const mediaStream = await navigator.mediaDevices.getUserMedia({
 					audio: true,
 					video: false,
+                    autoGainControl: false,
+                    echoCancellation: false,
+                    noiseSuppression: false
 				});
 				setPermission(true);
 				setStream(mediaStream);
@@ -39,6 +48,7 @@ const AudioRecorder = () => {
 
 	const handleRecordingStart = async () => {
 		setRecordingStatus("recording");
+
 		const media = new MediaRecorder(stream, { type: mimeType });
 
 		mediaRecorder.current = media;
@@ -54,6 +64,7 @@ const AudioRecorder = () => {
 		};
 
 		setAudioChunks(localAudioChunks);
+        SpeechRecognition.startListening();
        
 	};
     
@@ -79,9 +90,30 @@ const AudioRecorder = () => {
 
 			setAudioChunks([]);
 		};
+        SpeechRecognition.stopListening();
 	};
-
-
+   
+    // const handleRecognition = () => {
+    //     const recognition = new window.webkitSpeechRecognition();
+    //     recognition.lang = 'en-US';
+    //     recognition.continuous = true;
+    //     recognition.interimResults = true;
+    //     recognition.start();
+    //     recognition.onresult=function(event) {
+    //         let result = ''
+    //         for(let i = event.resultIndex;i <= event.resultIndex; i++) {
+    //             if (event.results[i].isFinal) {
+    //                 result += event.results[i][0].transcript;
+    //             }
+    //         }
+    //         console.log(result,'res')
+    //         setConvertedText(result);
+    //     }
+    //      recognition.stop();
+    //      recognition.onend = function() {
+    //      console.log('disconnected')
+    //      }
+    // }
 
 	return (
 		<div className="container">
@@ -96,28 +128,21 @@ const AudioRecorder = () => {
                 <button onClick={handleRecordingResume}>
                     Resume
                 </button>
-                {/* {permission && recordingStatus === "inactive" ? (
-                    <button onClick={startRecording} type="button">
-                        Record
-                    </button>
-                ) : null} */}
                 <button onClick={handleRecordingStop}>
                     Stop
                 </button>
 			</div>
-				{audio ? (
-					<div className="audio-player">
-						<audio src={audio} controls></audio>
-                        <button className="convert-btn">
-                            Covert to text
-					    </button>
-						{/* <a download href={audio}>
-							Download Recording
-						</a> */}
-					</div>
+            {audio ? (
+                <div className="audio-player">
+                    <audio src={audio} controls />
+                    <div className="transcribed-text">
+                        {transcript}
+                    </div>
+                </div>
 				) : null}
 		</div>
 	);
 };
+
 
 export default AudioRecorder;
